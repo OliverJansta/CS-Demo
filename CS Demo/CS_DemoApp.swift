87960f02a2b8eb8dc302897a0ab3fdf6bcfefbff
Oliver Jansta <oliverjansta@gmail.com>
2022-06-23 14:07:30 +0200

    Initial Commit

---
+++ CS Demo/CS_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS_DemoApp.swift
+//  CS Demo
+//
+//  Created by Oliver Jansta on 23.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CS_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
